0070104aaff7a330ffa36ec210bc92a1e988e375
Deep Patel <dpatel39@students.towson.edu>
2019-02-28 18:06:42 -0500

    Game fully implemented with few visual additions

--- Program2.js
+++ Program2.js
@@ -2,7 +2,8 @@ var gameData = {
 	roundsToPlay: 0,
 	roundsRemaining: 0,
 	humanWins: 0,
-	computerWins: 0
+	computerWins: 0,
+	ties: 0.
 };
 
 
@@ -33,36 +34,110 @@ function userChoice(choice){
 	// round winner is: human, computer or tie
 	var roundWinner = getRoundWinner(userChoiceText, compChoiceText);
 	
-	if(roundWinner == "HUMAN"){
+	if(roundWinner == "user"){
 		gameData.humanWins ++
+		getElem("HUMAN_WINS").innerHTML = 
+			gameData.humanWins;
+	}
+	else if(roundWinner == "computer"){
+		gameData.computerWins ++
+		getElem("COMPUTER_WINS").innerHTML = 
+			gameData.computerWins;
 	}
 	else{
-		gameData.computerWins ++
+		getElem("TIES").innerHTML = 
+			++ gameData.ties;
 	}
 	
+	getElem("COMP_CHOICE").innerHTML = 
+		compChoiceText;
+	getElem("USER_CHOICE").innerHTML = 
+		userChoiceText;
+	getElem("WINNER").innerHTML = 
+		roundWinner;
 	
-	if(gameData.roundsRemaining < 1){
-		alert("Game over !");
-		initGame();
-		showStartBtn();
-	}
 	
 	gameData.roundsRemaining --;
 	
 	getElem("ROUNDS_REM").innerHTML = 
 		gameData.roundsRemaining;
+		
+		
+	// Check to see if the game is over.
+    if (gameData.roundsRemaining < 1) {
+		getElem("CHOICE_BTNS").hidden = true; 
+		showResults(gameData.humanWins, gameData.computerWins, gameData.ties);
+	}
 }
 
+
+// Function to get the random computer choice. 
 function getCompChoice(){
     var choices = ["rock","paper","scissor","lizard","spock"]
     return choices[Math.floor(Math.random()* 5)];
 }
 
 
+// TODO: Implement Round Winner Function:
+// Since there are three possibilities (either play can win, or it could be a tie)...:
+function getRoundWinner(user, computer) {
+	var winner;
+	if(user == computer){
+		winner = "tie";
+	}
+	else if(user == "lizard" && computer == "spock" ||
+	   user == "spock" && computer == "scissors" ||
+	   user == "scissors" && computer == "paper" ||
+	   user == "paper" && computer == "rock" ||
+	   user == "rock" && computer == "lizard" ||
+	   user == "scissors" && computer == "lizard" ||
+	   user == "lizard" && computer == "paper" ||
+	   user == "rock" && computer == "scissors" ||
+	   user == "spock" && computer == "rock" ||
+	   user == "paper" && computer == "spock"){
+		   
+		winner = "user";
+		
+	}
+	else {
+		winner = "computer";
+	}
+	
+	return winner;
+}
+
+
 // Function to initialize the game elements
 function initGame() {
+	gameData.roundsToPlay = 0;
+	gameData.roundsRemaining = 0;
+	gameData.humanWins = 0;
+	gameData.computerWins = 0;
+	gameData.ties = 0;
+	getElem("COMPUTER_WINS").innerHTML = 0;
+	getElem("HUMAN_WINS").innerHTML = 0;
+	getElem("TIES").innerHTML = 0;
+	getElem("CHOICE_BTNS").hidden = false; 
+	getElem("ROUNDS_REM").hidden = false;
     hideAll();
+	getElem("WINNER").innerHTML = ""; 
     showInstructions();
+}
+
+function showResults(userWins, compWins, totalTies){
+	getElem("RESULT_FIELD").hidden = false;
+	getElem("ROUNDS_REM").hidden = true; 
+	
+	if(userWins > compWins){
+		getElem("FINAL_WINNER").innerHTML = "You won, Have fun !";
+	}
+	else if(userWins < compWins){
+		getElem("FINAL_WINNER").innerHTML = "Computer won, But you played well !";
+		
+	}
+	else{
+		getElem("FINAL_WINNER").innerHTML = "It's a tie, You both rock !";
+	}
 }
 
 // Hides all interface elements.
@@ -70,20 +145,23 @@ function hideAll() {
     getElem("INSTRUCTIONS").hidden = true;
     getElem("NO_OF_ROUNDS").hidden = true;
     getElem("PLAY_FIELD").hidden = true;
+	getElem("RESULT_FIELD").hidden = true;
 }
 
-//when the page loads
+// when the page loads
 function showInstructions(){
 	getElem("INSTRUCTIONS").hidden = false;
 }
 
-function showStartBtn(){
-	getElem("START_GAME_BTN").hidden = false;
+// hide playfield
+function hidePlayfield(){
+	getElem("PLAY_FIELD").hidden = true;
 }
+
 
 //when clicked start game
 function showRounds(){
-	getElem("START_GAME_BTN").hidden = true;
+	getElem("INSTRUCTIONS").hidden = true;
 	getElem("NO_OF_ROUNDS").hidden = false;
 }
 
@@ -91,13 +169,11 @@ function showRounds(){
 function showPlay(){
 	getElem("NO_OF_ROUNDS").hidden = true;
 	getElem("PLAY_FIELD").hidden = false;
+	getElem("RESULT_FIELD").hidden = true;
 	
 	gameData.roundsToPlay = getElem("ROUNDS_TO_PLAY").value;
 	gameData.roundsRemaining = getElem("ROUNDS_TO_PLAY").value;
 	
 	getElem("ROUNDS_REM").innerHTML = gameData.roundsRemaining;
-	
-	
-	
 
 }
